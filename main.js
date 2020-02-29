802bdcd6b3c702f2384a8b124a059a0b43201744
deer000 <camel000k@gmail.com>
2020-02-29 16:55:38 +0900

    calcurator

---
+++ main.js
@@ -0,0 +1,13 @@
+var deer = 55;
+var cat = 110;
+var oper = "*";
+
+if (oper === "+") {
+  console.log(deer + cat);
+} else if (oper === "-") {
+  console.log(deer - cat);
+} else if (oper === "*") {
+  console.log(deer * cat);
+} else if (oper === "/") {
+  console.log(deer / cat);
+}
